8a8f4f59e1db4eb682084c815bad8f53cd723448
Dayal Gutiérrez <dayal.gutierrez5863@alumnos.udg.mx>
2020-09-17 10:30:36 -0500

    Creacion de la primera parte (captura suma y promedio)

--- main.c
+++ main.c
@@ -1,5 +1,25 @@
 # include <stdio.h>
 
+void capturar_enteros(){
+    int enteros[5],suma=0;
+    float promedio;
+    for (size_t i = 0; i < 5; i++)
+    {
+        scanf("%d", &enteros[i]);
+    }
+    for (size_t i = 0; i < 5; i++)
+    {
+        printf("%d\n", enteros[i]);
+    }
+    for (size_t i = 0; i < 5; i++)
+    {
+        suma += enteros[i];
+    }
+    printf("Suma: %d\n", suma);
+    printf("Promedio: %f\n", suma / 5.0);
+
+}
+
 int main(){
     char op;
 
@@ -11,6 +31,21 @@ int main(){
         scanf("%c", &op);
 
         fflush(stdin);
+
+        switch (op)
+        {
+        case '1':
+            capturar_enteros();
+            break;
+        case '2':
+            /* code */
+            break;
+        case '3':
+            /* code */
+            break;
+        default:
+            break;
+        }
 
     } while (op != '0');
 }
